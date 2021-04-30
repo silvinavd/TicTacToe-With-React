75cd8fbdaa00ec14c0d3c5b43fa88d470324d27c
silvinavd <silvinavalverde8@gmail.com>
2021-04-30 19:59:08 +0000

    bbutton

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
+export function Home(props) {
+	const [count, setCount] = useState(0);
 	return (
 		<div className="container-fluid content">
 			<div className="row border-bottom-dark">
-				<div className="col-4 tic-box"> </div>
+				<div className="col-4 tic-box">
+					{" "}
+					<p>{count}</p>
+					<button
+						onClick={() => setCount("X")}
+						className="btn btn-primary fixed-top">
+						X player
+					</button>
+				</div>
 				<div className="col-4 border-left border-right border-dark tic-box"></div>
 				<div className="col-4 tic-box"> </div>
 			</div>
