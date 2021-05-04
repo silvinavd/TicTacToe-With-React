33903f8ff5a36ec3005c28d2a8fe81df37b34fc2
silvinavd <silvinavalverde8@gmail.com>
2021-05-04 14:17:48 +0000

    validacion

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -18,7 +18,53 @@ export function Home(props) {
 	]);
 
 	const [player, setPlayer] = useState("X");
+	const [winner, setWinner] = useState("");
 
+	useEffect(() => {
+		validarGanador();
+		if (winner != "") {
+			alert("Winner is: " + winner);
+		}
+	});
+
+	function validarGanador() {
+		for (let i = 0; i < game.length; i = i + 3) {
+			if (
+				game[i] != " " &&
+				game[i] == game[i + 1] &&
+				game[i] == game[i + 2]
+			) {
+				setWinner(game[i]);
+			}
+		}
+		for (let i = 0; i < game.length; i = i + 1) {
+			if (
+				game[i] != " " &&
+				game[i] == game[i + 4] &&
+				game[i] == game[i + 8]
+			) {
+				setWinner(game[i]);
+			}
+		}
+		for (let i = 0; i < game.length; i = i + 2) {
+			if (
+				game[i] != " " &&
+				game[i] == game[i + 2] &&
+				game[i] == game[i + 4]
+			) {
+				setWinner(game[i]);
+			}
+		}
+		for (let i = 0; i < game.length; i = i + 1) {
+			if (
+				game[i] != " " &&
+				game[i] == game[i + 3] &&
+				game[i] == game[i + 6]
+			) {
+				setWinner(game[i]);
+			}
+		}
+	}
 	const marcar = i => {
 		let cambioTurno = false;
 		let newGame = game.map((e, index) => {
